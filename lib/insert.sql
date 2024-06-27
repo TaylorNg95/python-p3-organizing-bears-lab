d004c13fa95c4e76766f0792b9ea9fec4f848609
Taylor Ng <taylormng95@gmail.com>
2024-06-27 11:39:26 -0400

    completes SQL lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate', 10, 'M', 'brown', 'happy', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Rowdy', 20, 'F', 'polar', 'rowdy', 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Tabitha', 30, 'M', 'polar', 'sleepy', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Sergeant Brown', 40, 'F', 'brown', 'funny', 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Melissa', 50, 'M', 'black', 'alright', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Grinch', 60, 'F', 'black', 'mean', 0);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Wendy', 70, 'M', 'brown', 'happy', 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(NULL, 80, 'F', 'brown', 'weird', 0);
